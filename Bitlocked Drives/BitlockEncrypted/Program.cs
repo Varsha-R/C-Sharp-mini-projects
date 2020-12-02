c6ff0a820fc17fac55372239bdad99a12e24419a
Varsha Ravindra <varsharavindra12@gmail.com>
2020-12-02 14:24:27 +0530

    Added code for bitlocked drives check
    The code checks if the drives present on the system (Windows) are bitlock encrypted. It returns 0 if the drives are not bitlocked.

---
+++ Bitlocked Drives/BitlockEncrypted/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Management;
+
+namespace BitlockEncrypted
+{
+    class Program
+    {
+        private static void TestBitLockerMenuItem_Click()
+        {
+            var path = new ManagementPath(@"\ROOT\CIMV2\Security\MicrosoftVolumeEncryption")
+            { ClassName = "Win32_EncryptableVolume" };
+            var scope = new ManagementScope(path);
+            path.Server = Environment.MachineName;
+            var objectSearcher = new ManagementClass(scope, path, new ObjectGetOptions());
+            foreach (var item in objectSearcher.GetInstances())
+            {
+                Console.WriteLine(item["DriveLetter"].ToString() + " | | " + item["ProtectionStatus"].ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TestBitLockerMenuItem_Click();
+            Console.ReadKey();
+        }
+    }
+}
