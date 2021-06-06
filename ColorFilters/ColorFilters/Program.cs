16a343406ff348fe4f34bdbbf48de7c115380d8c
Varsha Ravindra <varsharavindra12@gmail.com>
2021-06-06 10:42:21 +0530

    Added project files
    1. Added code for fetching color filters from registry.
    2. Added code for finding intersection of two lists.

---
+++ ColorFilters/ColorFilters/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorFilters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryKey colorFilter = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\ColorFiltering", true);
+            string filter = colorFilter.GetValue("Active").ToString();
+            colorFilter.SetValue("Active", "0", RegistryValueKind.DWord);
+            string cmdPath = Environment.GetEnvironmentVariable("ComSpec");
+            Console.WriteLine(filter);
+            Console.ReadKey();
+        }
+    }    
+}
---
+++ CommonMatch/CommonMatch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonMatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> old = new List<string>
+            {
+                "varsha.ravindra@outlook.com",
+                "varsha.ravindra@xyz.com"
+            };
+
+            List<string> newS = new List<string>
+            {
+                "varsha.ravindra@xyz.com"
+            };
+
+            IEnumerable<string> commons = Enumerable.Empty<string>();
+            commons = old.Intersect(newS);
+            foreach(string common in commons)
+            {
+                Console.WriteLine(common);
+            }
+            Console.ReadKey();
+        }
+    }
+}
