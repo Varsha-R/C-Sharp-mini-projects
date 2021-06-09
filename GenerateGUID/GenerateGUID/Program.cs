53194c50b85b6cf850f1d43bc832e74b588c92b4
Varsha Ravindra <varsharavindra12@gmail.com>
2021-06-09 10:27:39 +0530

    Added project files
    1. Generating GUID
    2. Fetching system folder path

---
+++ GenerateGUID/GenerateGUID/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateGUID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random random = new Random();
+            string characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-";
+            uint maxValue = (uint)((((ulong)uint.MaxValue) + 1) / (uint)characters.Length * (uint)characters.Length);
+            int length = 12;
+            StringBuilder result = new StringBuilder(12);
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    byte[] buffer = new byte[sizeof(uint)];
+                    rng.GetBytes(buffer);
+                    uint num = BitConverter.ToUInt32(buffer, 0);
+                    result.Append(characters[(int)(num % (uint)characters.Length)]);
+                }
+            }
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    result.Append(characters[random.Next(characters.Length)]);
+            //}
+            Console.WriteLine(result.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GetFolder/GetFolder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\Mozilla\\Firefox\\Profiles";
+            Console.WriteLine(path);
+            Console.WriteLine(Directory.GetAccessControl(path));
+            if(Directory.Exists(path))
+            {
+                Console.WriteLine("Exists");
+            }
+            else
+            {
+                Console.WriteLine("Does not exist");
+            }
+            string defaultFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Mozilla\\Firefox\\Profiles");
+            Console.WriteLine(defaultFolder);
+            Console.ReadKey();
+        }
+    }
+}
