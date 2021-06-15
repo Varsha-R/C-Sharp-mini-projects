85693ebbb4ae8417d6a57e661859298f34d5de03
Varsha Ravindra <varsharavindra12@gmail.com>
2021-06-15 13:58:14 +0530

    Added project files
    1. POC for securing entitlements (encrypting(
    2. Sector write progress calculator

---
+++ SectorWriteProgress/SectorWriteProgress/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace SectorWriteProgress
+{
+    class ProgressBar
+    {
+        private int _lastOutputLength;
+        private readonly int _maximumWidth;
+
+        public ProgressBar(int maximumWidth)
+        {
+            _maximumWidth = maximumWidth;
+            Show(" [ ");
+        }
+
+        public void Update(double percent)
+        {
+            // Remove the last state           
+            string clear = string.Empty.PadRight(_lastOutputLength, '\b');
+            Show(clear);
+
+            // Generate new state           
+            try
+            {
+                int width = (int)(percent / 100 * _maximumWidth);
+                int fill = _maximumWidth - width;
+                string output = string.Format("{0}{1} ] {2}%", string.Empty.PadLeft(width, '='), string.Empty.PadLeft(fill, ' '), percent.ToString("0.0"));
+                Show(output);
+                _lastOutputLength = output.Length;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void Show(string value)
+        {
+            Console.Write(value);
+        }
+
+        public static void DrawTextProgressBar(int progress, long total)
+        {
+            //draw empty progress bar
+            Console.CursorVisible = false;
+            Console.CursorLeft = 0;
+            Console.Write("["); //start
+            Console.CursorLeft = 32;
+            Console.Write("]"); //end
+            Console.CursorLeft = 1;
+            float onechunk = 30.0f / total;
+
+            //draw filled part
+            int position = 1;
+            for (int i = 0; i < onechunk * progress; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.CursorLeft = position++;
+                Console.Write(" ");
+            }
+
+            //draw unfilled part
+            for (int i = position; i <= 31; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.CursorLeft = position++;
+                Console.Write(" ");
+            }
+
+            //draw totals
+            Console.CursorLeft = 35;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            float progressPercent = (int)(((float)progress / total) * 100);
+            Console.Write(progressPercent.ToString() + "%");
+            //Console.Write(progress.ToString() + " of " + total.ToString() + "    "); //blanks at the end remove any excess
+        }
+
+        public static void Main()
+        {
+            long totalCount = 100;
+            for (int i = 0; i <= totalCount; i++)
+            {
+                DrawTextProgressBar(i, totalCount);
+                Console.Write("\r");
+            }
+            Console.ReadKey();
+            //var progress = new ProgressBar(100);
+            //for (int i = 0; i < 100000; i++)
+            //{
+            //    progress.Update(i);
+            //    System.Threading.Thread.Sleep(50);
+            //}
+            //Console.WriteLine();
+        }
+    }
+}
---
+++ SecureEntitlementPOC/SecureEntitlementPOC/Program.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SecureEntitlementPOC
+{
+    class Program
+    {
+        static readonly string entitlementTestString = "rutf-4954fjskcn-#$@#Ofdsk){DSsdDET^U";
+        static readonly byte[] initializationVector = new byte[] { 162, 244, 37, 135, 24, 63, 34, 247, 57, 183, 218, 183, 210, 234, 240, 91 };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Original string: {0}", entitlementTestString);
+            long stringHash = GetHash(entitlementTestString);
+            Console.WriteLine("Hash: {0}", stringHash);
+
+            string uuid = GetUUID();
+            uuid = uuid.Replace("-", "");
+            Console.WriteLine("UUID: {0}", uuid);
+
+            var encryptedString = Crypt.EncryptString(uuid, entitlementTestString, initializationVector);
+            Console.WriteLine("Encrypted string: {0}", encryptedString);
+
+            uuid = uuid.Replace("4", "5");
+            Console.WriteLine("UUID AFTER tampering: {0}", uuid);
+
+            var decryptedString = Crypt.DecryptString(uuid, encryptedString, initializationVector);
+            Console.WriteLine("Decrypted string: {0}", decryptedString);
+
+            #region Storing details in DB
+
+            SQLiteConnection.CreateFile("TestDatabase.sqlite");
+
+            SQLiteConnection openDBConnection = new SQLiteConnection("Data Source=TestDatabase.sqlite");
+            openDBConnection.Open();
+
+            string sql = "create table Entitlement (entitlementString varchar(100), entitlementHash long)";
+            SQLiteCommand command = new SQLiteCommand(sql, openDBConnection);
+            command.ExecuteNonQuery();
+
+            string insert = "insert into Entitlement (entitlementString, entitlementHash) values (@encryptedString, @stringHash)";
+            SQLiteCommand insertCommand = openDBConnection.CreateCommand();
+            insertCommand.CommandText = insert;
+            insertCommand.Parameters.AddWithValue("@encryptedString", encryptedString);
+            insertCommand.Parameters.AddWithValue("@stringHash", stringHash);
+            insertCommand.ExecuteNonQuery();
+            insertCommand.Dispose();
+            openDBConnection.Close();
+
+            #endregion
+
+            Console.ReadKey();
+        }
+
+        static string GetUUID()
+        {
+            string uuid = string.Empty;
+
+            ManagementClass mc = new ManagementClass("Win32_ComputerSystemProduct");
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            foreach (ManagementObject mo in moc)
+            {
+                uuid = mo.Properties["UUID"].Value.ToString();
+                break;
+            }
+            
+            if(uuid.Equals("FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"))
+            {
+                RegistryKey root;
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    root = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                }
+                else
+                {
+                    root = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+                }
+                RegistryKey cryptography = root.OpenSubKey(@"SOFTWARE\Microsoft\Cryptography", true);
+                if (cryptography.GetValueNames().Contains("MachineGuid"))
+                {
+                    uuid = cryptography.GetValue("MachineGuid").ToString();
+                }
+                else
+                {
+                    Console.WriteLine("Could not fetch machine guid from registry");
+                }                
+            }
+            return uuid;
+        }
+
+        static long GetHash(string text)
+        {
+            var hash = 23;
+            foreach (char c in text)
+            {
+                hash = hash * 31 + c;
+            }
+            return hash;
+        }
+    }
+}
